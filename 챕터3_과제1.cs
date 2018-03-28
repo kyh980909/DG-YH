e530f841d67449d0d3bb9cb130e31e8047204f8c
kyh980909 <kyh980909@naver.com>
2018-03-29 01:29:24 +0900

    챕터3 과제

---
+++ 챕터3_과제1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch3_과제1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year; //사용자에게 년도 입력 받을 변수
+
+            Console.Write("생년을 입력하세요: ");
+            year = int.Parse(Console.ReadLine());
+
+            switch (year % 12)
+            {
+                case 0:
+                    Console.WriteLine("원숭이띠 입니다.");
+                    break;
+                case 1:
+                    Console.WriteLine("닭띠 입니다.");
+                    break;
+                case 2:
+                    Console.WriteLine("개띠 입니다.");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지띠 입니다.");
+                    break;
+                case 4:
+                    Console.WriteLine("쥐띠 입니다.");
+                    break;
+                case 5:
+                    Console.WriteLine("소띠 입니다.");
+                    break;
+                case 6:
+                    Console.WriteLine("범띠 입니다.");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼띠 입니다.");
+                    break;
+                case 8:
+                    Console.WriteLine("용띠 입니다.");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀띠 입니다.");
+                    break;
+                case 10:
+                    Console.WriteLine("말띠 입니다.");
+                    break;
+                case 11:
+                    Console.WriteLine("양띠 입니다.");
+                    break;
+            }
+        }
+    }
+}
---
+++ 챕터3_과제2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch3_과제2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month;
+
+            Console.Write("몇 월 인가요? ");
+            month = int.Parse(Console.ReadLine());
+
+            switch (month)
+            {
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄 입니다.");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름 입니다.");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을 입니다.");
+                    break;
+
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울 입니다.");
+                    break;
+
+                default:
+                    Console.WriteLine("잘못입력하셨습니다.");
+                    break;
+            }
+        }
+    }
+}
