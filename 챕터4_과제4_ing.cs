1e8b3c1084558106fc26f8faa0bbdb649aca576b
김용호 <kyh980909@gmail.com>
2018-04-03 01:15:15 +0900

    챕터4_과제4
    개미수열 푸는중

--- 챕터4_과제4_ing.cs
+++ 챕터4_과제4_ing.cs
@@ -4,47 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ch4_과제4
+namespace 개미수열
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] arrayA = new string[500];
-            int[] arrayB = new int[500];
+            string[ , ] arrA = new string[20, 10]; // 수열을 저장할 2차원 배열
 
-            int num = 1; // 수열을 저장할 변수
-            int cnt = 0; // 카운트 변수
-            int j = 0, k = 0;   // k는 arrayA의 인덱스
-            int newNum = 0;
+            int k;
+            int cnt; // 같은 숫자가 몇번 나왔는지 세는 변수
+            bool newNum = false; //새로운 숫자가 나오는지 구별 할 변수
+
+            arrA[0, 0] = "1"; // 첫번째 수열은 1
 
             for (int i = 0; i < 20; i++)
             {
-                arrayA[i] = num.ToString("0");    //첫 번째 수열: 1
-
-                while (arrayA[j] != null)
+                k = 0;
+                cnt = 1;
+                while (k < 9 && (arrA[i, k] != null))  //1. 배열 인덱스 범위 지정 2. null을 만나면 반복문 종료
                 {
-                    newNum = 0;     // 이전 숫자와 다음 숫자가 다를 경우가 몇번 나왔는지 세는 변수
-                    cnt = 0;        // 같은 숫자가 몇개인지 세는 변수
-                    while (arrayA[k] != null && arrayA[k] != arrayA[k + 1])  // 1. null값을 만나면 반복문 탈출
-                    {                                                        // 2. 다른 숫자가 나올때 까지 반복                  
-                        cnt++;
-                        k++;
-                        if (cnt > 1)
-                        {
-                            newNum++;
-                        }
-                        if (newNum < 2)
-                        {
-                            j++;
-                            arrayA[j] = cnt.ToString();
-                        }
-                        else if (newNum > 1)
-                        {
-                            arrayA[j] = cnt.ToString();
-                        }
-                    }
+                    if (i < 19)                                    // 배열 인덱스 범위 지정
+                        arrA[i + 1, k] = arrA[i, k];               // 이전 수열을 다음 수열에 복사 - 다음 수열에 이전 수열에서 추가된 값을 적용하기 위함
+                    if (k < 9 && (arrA[i, k] == arrA[i, k + 1]))   //1. 배열 인덱스 범위 지정
+                        cnt++;                                     //2. 현재 인덱스의 숫자와 다음 인덱스의 숫자가 같으면 cnt값 1 증가
+                    if (cnt > 1)                                   // 같은숫자가 2번 이상나오면 새로운 숫자가 나올 것을 암시
+                        newNum = true;                             // ?
+                    k++;
                 }
+                if (i < 19)                                        // 배열 인덱스 범위 지정
+                    arrA[i + 1, k] = cnt.ToString();
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(arrA[i, j]);
+                }
+                Console.WriteLine();
             }
         }
     }
