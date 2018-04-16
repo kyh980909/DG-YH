cb19fa06826c5798ce4d57147ee323e5f45f4c4b
kyh980909 <kyh980909@naver.com>
2018-04-16 16:56:19 +0900

    윈폼 예제
    고양이 키우기 간단한 윈폼 프로그램

---
+++ 고양이 키우기/Cat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 고양이_키우기
+{
+    public class Cat
+    {
+        public string Name;
+        public int Age;
+
+        private int Happiness = 50;
+
+        public Cat(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void Play()
+        {
+            Happiness = Happiness + 10;
+            if (Happiness > 100)
+                Happiness = 100;
+        }
+
+        public void Eat()
+        {
+            Happiness = Happiness + 20;
+            if (Happiness > 100)
+                Happiness = 100;
+        }
+
+        public string Express()
+        {
+            string message = "";
+
+            if (Happiness >= 80)
+                message = "I'm so happy.";
+            else if (Happiness >= 60)
+                message = "I'm happy.";
+            else if (Happiness >= 40)
+                message = "I'm so so.";
+            else if (Happiness >= 20)
+                message = "I'm gloomy.";
+            else
+                message = "I'm sad.";
+
+            return this.Name + " : " + message;
+        }
+
+        public void GetBord()
+        {
+            Happiness = Happiness - 10;
+
+            if (Happiness < 0)
+                Happiness = 0;
+        }
+    }
+}
---
+++ 고양이 키우기/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 고양이_키우기
+{
+    public partial class Form1 : Form
+    {
+        Cat MyCat = new Cat("Lucas", 1);
+        public Form1()
+        {
+            InitializeComponent();
+            CatScreen.Text = MyCat.Express();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            MyCat.Play();
+            CatScreen.Text = MyCat.Express();
+        }
+
+        private void Eat_Click(object sender, EventArgs e)
+        {
+            MyCat.Eat();
+            CatScreen.Text = MyCat.Express();
+        }
+
+        private void CatTimer_Tick(object sender, EventArgs e)
+        {
+            MyCat.GetBord();
+            CatScreen.Text = MyCat.Express();
+        }
+    }
+}
